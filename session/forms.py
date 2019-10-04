f62606852eb3dc4da7273a4d03cca33665a47e08
Javier <jfbahamondes@uc.cl>
2019-10-04 02:24:36 -0300

    details css register

--- session/forms.py
+++ session/forms.py
@@ -4,18 +4,39 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    passwordAttrs = {
+        "type": "password",
+        "autocomplete": "new-password",
+    }
+    textAttrs = {
+        "type": "text",
+        "autocomplete": "on",
+    }
+    emailAttrs = {
+        "type": "email",
+        "autocomplete": "on",
+    }
     first_name = forms.CharField(
-        max_length=30, required=False, label='Nombre')
+        max_length=30, required=False, label='Nombre',
+        widget=forms.TextInput(attrs=textAttrs))
     last_name = forms.CharField(
-        max_length=30, required=False, label='Apellido')
+        max_length=30, required=False, label='Apellido',
+        widget=forms.TextInput(attrs=textAttrs))
     username = forms.CharField(
-        max_length=30, required=True, label='Nombre de Usuario')
-    email = forms.EmailField(
+        max_length=30, required=True, label='Nombre de Usuario',
+        widget=forms.TextInput(attrs=textAttrs))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs=emailAttrs),
         max_length=254, )
-    password1 = forms.CharField(
-        widget=forms.PasswordInput(), label='Contraseña', )
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs=passwordAttrs), label='Contraseña', )
     password2 = forms.CharField(widget=forms.PasswordInput(
-    ), label='Confirmar Contraseña', )
+        attrs=passwordAttrs), label='Confirmar Contraseña', )
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = User
@@ -26,3 +47,8 @@ class SignUpForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(label="Nombre de Usuario")
     password = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
